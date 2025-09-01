edc102662ef3be7502be2e26b9d10705ee28eec6
Atluri Dhanush <atluridhanu129@gmail.com>
2025-09-01 13:20:33 +0530

    Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the desired password length: ");
+        int length = scanner.nextInt();
+
+        System.out.print("Include uppercase letters? (true/false): ");
+        boolean useUpper = scanner.nextBoolean();
+
+        System.out.print("Include lowercase letters? (true/false): ");
+        boolean useLower = scanner.nextBoolean();
+
+        System.out.print("Include numbers? (true/false): ");
+        boolean useDigits = scanner.nextBoolean();
+
+        System.out.print("Include special characters? (true/false): ");
+        boolean useSpecial = scanner.nextBoolean();
+
+        String password = PasswordGenerator.generatePassword(length, useUpper, useLower, useDigits, useSpecial);
+        System.out.println("Generated Password: " + password);
+
+        scanner.close();
+    }
+}
